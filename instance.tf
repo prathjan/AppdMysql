6deaacb28441a5aeb4da8700f24b7401b929967e
prathjan <prathjan@cisco.com>
2021-08-19 13:01:29 -0700

    only name

--- instance.tf
+++ instance.tf
@@ -91,8 +91,8 @@ resource "vsphere_virtual_machine" "vm_deploy" {
 }
 
 
-output "vm_deploy" {
-  value = [vsphere_virtual_machine.vm_deploy.*.name, vsphere_virtual_machine.vm_deploy.*.default_ip_address]
+output "vm_name" {
+  value = vsphere_virtual_machine.vm_deploy.*.name
 }
 
 
