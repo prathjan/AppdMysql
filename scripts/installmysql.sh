a430a8394c00c7c15e6c5c46aa03a0ca9937a461
prathjan <prathjan@cisco.com>
2021-08-19 21:55:24 -0700

    fixes

--- scripts/installmysql.sh
+++ scripts/installmysql.sh
@@ -1,6 +1,6 @@
 echo $1
 sudo apt update
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '$1''
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '$1''
+sudo 'debconf-set-selections <<< mysql-server mysql-server/root_password password '$1''
+sudo 'debconf-set-selections <<< mysql-server mysql-server/root_password_again password '$1''
 sudo apt-get -y install mysql-server
 
